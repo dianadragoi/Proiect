e148486b136506cb9a3f45380c732396c78e1cbd
Cirstea Radu <raducirstea2706@gmail.com>
2016-01-07 10:35:18 +0200

    Modificare Comment
    //radu
    //diana

--- WindowsFormsApplication_PROIECT/WindowsFormsApplication_PROIECT/Program.cs
+++ WindowsFormsApplication_PROIECT/WindowsFormsApplication_PROIECT/Program.cs
@@ -17,6 +17,8 @@ namespace WindowsFormsApplication_PROIECT
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //radu
+            //diana
         }
     }
 }
